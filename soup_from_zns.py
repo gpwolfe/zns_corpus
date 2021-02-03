e7e4f8ac5b01769c9694affd36f8b8e95e4b3d2a
Gregory <setantaontherock1@gmail.com>
2021-02-03 16:57:13 +0100

    get_text and get_raw_text automatically remove the privacy notice element

--- soup_from_zns.py
+++ soup_from_zns.py
@@ -23,7 +23,7 @@ class InputError(Exception):
 
 
 
-class site_text:
+class SiteText:
     """
     Pass site URL as a string
     """
@@ -34,11 +34,16 @@ class site_text:
          self.site_html = requests.get(self.site)
          self.site_soup = BeautifulSoup(self.site_html.content, 
                                         'html.parser')
-         self.text = ""
+         self.text = "No processed text yet. Run SiteText.get_text()"
+         self.raw_text = "No raw text yet. Run SiteText.get_raw_text()"
     
         
-    def get_text_raw(self):
+    def get_raw_text(self, remove_privacy_notice = True):
         """
+        
+
+        Parameters
+        ----------
         
 
         Returns
@@ -46,31 +51,50 @@ class site_text:
         None.
 
         """
+        # Removing the privacy notice corresp. to last <p> element
+        if remove_privacy_notice:
+            privacy_notice = re.compile("akismet\w*")
+            rem_element = self.site_soup.find_all(attrs={"class":privacy_notice})
+            removed = []
+            for i in rem_element:
+                removed = i.extract()
         
         all_ps = self.site_soup.find_all('p')
-        all_ps_text = [x.get_text() for x in all_ps]
-        texts_together = ' '.join(all_ps_text)
-        return texts_together
-        
+        all_ps = all_ps.extract()
+        all_ps = [x.get_text() for x in all_ps]
+        texts_together = ' '.join(all_ps)
+        self.raw_text = texts_together
+
+
     
     
-    def get_text(self):
+    def get_text(self, remove_privacy_notice = True):
         """
         Removes punctuation and extra spaces from the text and transforms all
         words to lowercase.
         
         Parameters
         ----------
-        text : List
-            A list of words from get_text. 
+        remove_privacy_notice : boolean, optional
+            Removes the common privacy notice on ZNS website news pages. Set
+            to False if you want to keep the privacy notice in the processed 
+            text.
+            The default is True.
 
 
         Returns
         -------
         Updates self.text with a string of all text held within the <p> tags
-        of the URL passed to the class instance.
+        of the URL passed to the class instance, minus the privacy notice if
+        remove_privacy_notice is set to True (default).
 
         """
+        if remove_privacy_notice:
+            privacy_notice = re.compile("akismet\w*")
+            rem_element = self.site_soup.find_all(attrs={"class":privacy_notice})
+            removed = []
+            for i in rem_element:
+                removed = i.extract()        
         all_ps = self.site_soup.find_all('p')
         all_ps_text = [x.get_text() for x in all_ps]
         texts_together = ' '.join(all_ps_text)
