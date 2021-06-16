3d3d1b2475fa7f0341b6830700a891342fd50c89
Gregory <setantaontherock1@gmail.com>
2021-06-16 21:47:55 +0200

    Improve text processing, analyzed-token csv file output

--- zns_spacy_to_csv.py
+++ zns_spacy_to_csv.py
@@ -2,13 +2,30 @@
 @author: gpwolfe
 """
 import csv
-import pandas as pd
 import spacy
 
 nlp = spacy.load('/Users/piper/opt/anaconda3/envs/pyling/lib/python3.8/site-packages/en_core_web_md/en_core_web_md-2.3.1')
 SPAM_WARN = 'This site uses Akismet to reduce spam.'
 
 def process_text(fn, fn_out):
+    """
+    Remove duplicates, captions (short-length text) and spam notice.
+    
+    Generates a new csv file with duplicates, short texts and spam notice
+    removed.
+
+    Parameters
+    ----------
+    fn : string
+        Input filename, csv file containing articles from scrapy.
+    fn_out : string
+        Output filename, csv file containing processed articles.
+
+    Returns
+    -------
+    None.
+
+    """
     with open(fn, 'r', newline='') as f:
         with open(fn_out, 'w', newline='') as out:
             reader = csv.reader(f)
@@ -40,27 +57,21 @@ def process_text(fn, fn_out):
 process_text('articles_10K.csv', 'articles_10K_out.csv')
 process_text('articles_100K.csv', 'articles_100K_out.csv')
 
-with open('articles_10K_out.csv') as f:
-    texts = []
-    for line in f:
-        texts.append(line)
-    for ix, article in enumerate(texts):
-        for ix2, article2 in enumerate(texts[ix+1:]):
-            if article == article2:
-                print('Duplicate', article[:20], article2[:20])
-        print(texts[0])
+def spacy_csv(fn_in, fn_out):
+    with open(fn_in, 'r') as f:
+        with open(fn_out, 'a', newline='') as out:
+            writer = csv.writer(out, delimiter=',')
+            writer.writerow(['token', 'lemma', 'pos', 'tag', 'is_stop_word'])
+            for line in f:
+                doc1 = nlp(line)
+                literals = [token.text for token in doc1]
+                lemmas = [token.lemma_ for token in doc1]
+                pos = [token.pos_ for token in doc1]
+                tags = [token.tag_ for token in doc1]
+                is_stop = [str(token.is_stop) for token in doc1]
+                for token_data in zip(literals, lemmas, pos, tags, is_stop):
+                    write_data = token_data
+                    writer.writerow(write_data)
 
-with open('articles_10K.csv', 'r') as f:
-    with open('10K_zns_corpus.csv', 'a', newline='') as out:
-        writer = csv.writer(out, delimiter=',')
-        for line in f:
-            doc1 = nlp(line)
-            literals = [token.text for token in doc1]
-            lemmas = [token.lemma_ for token in doc1]
-            pos = [token.pos_ for token in doc1]
-            tags = [token.tag_ for token in doc1]
-            is_stop = [str(token.is_stop) for token in doc1]
-            for token_data in zip(literals, lemmas, pos, tags):
-                write_data = token_data
-                writer.writerow(write_data)
-
+spacy_csv('articles_10K_out.csv', 'articles_10K_spacy.csv')
+spacy_csv('articles_100K_out.csv', 'articles_100K_spacy.csv')
